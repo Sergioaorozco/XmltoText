3ed431c86b7c1011fc65b89ed95353e10d21f6e1
Sergio <sergioa.orozcoo@gmail.com>
2022-11-19 14:43:35 -0500

    add Input Files

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let xmlInicio = document.getElementById('xmlOrigin');
 let xmlFin = document.getElementById('xmlDestino');
 let convertFile = document.getElementById('convertButton');
-let properties = document.getElementsByTagName('properties')
+let uploadedFile = document.getElementById('xmlFile')
 
 function xmlToText() {
   if(xmlInicio != "") {
@@ -10,8 +10,9 @@ function xmlToText() {
     let addNewLine = newText.replace(/<br\s*\/?>/g, '\n');
     let removeTags = addNewLine.replace(/\s*\<.*?\>\s*/g, '\n');
     xmlFin.value = removeTags
-  }
+  } 
 }
+
 
 convertFile.addEventListener('click', function(){
   xmlToText();
