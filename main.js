21fbf0549d0a113c4bf1d2dc8390a5ac39fdd08f
Sergio <sergioa.orozcoo@gmail.com>
2022-11-28 11:59:29 -0500

    Add Copy to clipboard icon to the tool

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 let xmlInicio = document.getElementById('xmlOrigin');
 let xmlFin = document.getElementById('xmlDestino');
 let convertFile = document.getElementById('convertButton');
-let uploadedFile = document.getElementById('xmlFile')
+let uploadedFile = document.getElementById('xmlFile');
+let copySong = document.getElementById('copySong')
+let tooltip = document.getElementById("myTooltip");
 
 function xmlToText() {
   if(xmlInicio != "") {
@@ -18,11 +20,32 @@ function xmlToText() {
     strDoc = strDoc.replace(/\n\n/g, '');
     strDoc = strDoc.replace(/\s*\n/i, '');
     xmlFin.value = strDoc
-    console.log(JSON.stringify(strDoc))
   } 
 }
 
 
 convertFile.addEventListener('click', function(){
   xmlToText();
+})
+
+convertFile.addEventListener('click', function(){
+  xmlFin.select()
+  xmlFin.setSelectionRange(0,9999)
+
+  // Copy the text inside
+  navigator.clipboard.writeText(xmlFin.value)
+})
+
+copySong.addEventListener('click', function(){
+  xmlFin.select()
+  xmlFin.setSelectionRange(0,9999)
+
+  // Copy the text inside
+  navigator.clipboard.writeText(xmlFin.value)
+  tooltip.innerHTML = "Your Song was copied!";
+})
+
+
+copySong.addEventListener(onmouseout, function(){
+  tooltip.innerHTML = "Copy to clipboard";
 })
